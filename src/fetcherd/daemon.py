7a50633856b2cdd2b44cf3e6d5f8945f941c240d
David Barnett <david@barnett.net.nz>
2015-07-16 12:42:48 +1200

    task scheduling

--- src/fetcherd/daemon.py
+++ src/fetcherd/daemon.py
@@ -3,9 +3,14 @@ import getpass
 import logging
 from logging import handlers
 
+from fetch import fetch
+from sort import sort
+import sources
+import providers
+
+from apscheduler.schedulers.background import BlockingScheduler
 
 def main(args, config):
-
     file = handlers.RotatingFileHandler(
         args['--log'],
         maxBytes=10485760,
@@ -17,6 +22,8 @@ def main(args, config):
 
     logging.getLogger('').addHandler(file)
 
+    scheduler = BlockingScheduler(logger=logging.getLogger('schedule'))
+
     logger = logging.getLogger('daemon')
     logger.info("Entered main")
 
@@ -25,3 +32,17 @@ def main(args, config):
     os.chdir(working_dir)
     logger.info("Working directory: {}".format(working_dir))
     logger.info("Running as user {}".format(getpass.getuser()))
+
+    provs = providers.get_providers()
+    loaded_sources = sources.get_sources()
+    current_souce = loaded_sources[config.source['class']](config.source['settings'])
+
+    scheduler.add_job(lambda: fetch(config, current_souce, provs),
+                      'cron', minute=30, id='fetch')
+    scheduler.add_job(lambda: sort(config, current_souce),
+                      'cron', minute=00, id='sort')
+
+    fetch(config, current_souce, provs)
+    sort(config, current_souce)
+
+    scheduler.start()
